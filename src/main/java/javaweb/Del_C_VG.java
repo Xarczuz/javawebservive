7b09a78a578490860c62fffbd6b16acf0b59873e
xarczuz <chjunchi.chiu@gmail.com>
2019-08-31 00:49:58 +0200

    autogen

--- src/main/java/javaweb/Del_C_VG.java
+++ src/main/java/javaweb/Del_C_VG.java
@@ -1,5 +1,16 @@
 package javaweb;
 
-public class Del_C_VG {
+import autogen.CountryInfoService;
+import autogen.CountryInfoServiceSoapType;
 
+public class Del_C_VG {
+	public static void main(String[] args) {
+		 
+		CountryInfoService service = new CountryInfoService();
+ 
+		CountryInfoServiceSoapType server = service.getCountryInfoServiceSoap();
+ 
+		System.out.println(server.listOfContinentsByCode().getTContinent().iterator().next().getSName());
+ 
+	}
 }
