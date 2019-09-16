5ee2b9ac7a51f702646fb78df2df9b43332fdfcf
Xarczuz <chjunchi.chiu@gmail.com>
2019-09-16 15:08:46 +0200

    cc

--- src/main/java/javaweb/Del_C_VG1.java
+++ src/main/java/javaweb/Del_C_VG1.java
@@ -1,6 +1,8 @@
 package javaweb;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -12,13 +14,18 @@ import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPMessage;
 
 /**
- * Uses the http protocol to send the xml message to the server and prints the response
+ * Uses the http protocol to send the xml message to the server and prints the
+ * response
+ * 
  * @author chjun
  *
  */
 public class Del_C_VG1 {
 
 	public static void main(String[] args) throws SOAPException {
+		
+		String ISOCODE ="US";
+		
 		String adress = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
 		URL url;
 		try {
@@ -37,7 +44,7 @@ public class Del_C_VG1 {
 				writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
 						+ "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"http://www.oorsprong.org/websamples.countryinfo\">\r\n"
 						+ "  <SOAP-ENV:Body>\r\n" + "    <ns1:FullCountryInfo>\r\n"
-						+ "      <ns1:sCountryISOCode>US</ns1:sCountryISOCode>\r\n" + "    </ns1:FullCountryInfo>\r\n"
+						+ "      <ns1:sCountryISOCode>"+ISOCODE+"</ns1:sCountryISOCode>\r\n" + "    </ns1:FullCountryInfo>\r\n"
 						+ "  </SOAP-ENV:Body>\r\n" + "</SOAP-ENV:Envelope>\r\n" + "");
 			} finally {
 				if (writer != null)
@@ -52,11 +59,18 @@ public class Del_C_VG1 {
 //			while ((res = reader.readLine()) != null) {
 //				System.out.println(res);
 //			}
-			
-			// Parse it to a SoapMEssage so that you can easier get you data from the tag names.
+
+			// Parse it to a SoapMEssage so that you can easier get you data from the tag
+			// names.
 			SOAPMessage sm = MessageFactory.newInstance().createMessage(new MimeHeaders(), con.getInputStream());
-			// Takes all the text content and prints it. 
-			System.out.println(sm.getSOAPBody().getTextContent());
+			// Takes all the text content and prints it.
+
+			System.out.println(
+					"Capital: " + sm.getSOAPBody().getElementsByTagName("m:sCapitalCity").item(0).getTextContent());
+			System.out.println("Country: " + sm.getSOAPBody().getElementsByTagName("m:sName").item(0).getTextContent());
+			System.out.println(
+					"Currency: " + sm.getSOAPBody().getElementsByTagName("m:sCurrencyISOCode").item(0).getTextContent());
+
 		} catch (MalformedURLException e) {// Catch errors
 			e.printStackTrace();
 		} catch (IOException e) {
