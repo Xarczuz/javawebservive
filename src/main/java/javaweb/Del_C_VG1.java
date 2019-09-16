5d1cdeda77fc262f82332721b279cd671a6aa2bd
Xarczuz <chjunchi.chiu@gmail.com>
2019-09-16 15:09:30 +0200

    cc

--- src/main/java/javaweb/Del_C_VG1.java
+++ src/main/java/javaweb/Del_C_VG1.java
@@ -1,8 +1,6 @@
 package javaweb;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
